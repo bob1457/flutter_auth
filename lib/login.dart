4eb80de93e729bc6131936222817e6d993e39292
Bob <bob.yuan@yahooo.com>
2021-09-10 21:52:42 -0700

    Completed UI Element

--- lib/login.dart
+++ lib/login.dart
@@ -7,6 +7,208 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool? isRememberMe = false;
+
+  Widget buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Email',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 6,
+                offset: Offset(0, 2),
+              )
+            ],
+          ),
+          height: 50,
+          child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(
+                color: Colors.black87,
+              ),
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 14),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Color(0xff5ac18e),
+                  ),
+                  hintText: 'Email',
+                  hintStyle: TextStyle())),
+        )
+      ],
+    );
+  }
+
+  Widget buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Password',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 6,
+                offset: Offset(0, 2),
+              )
+            ],
+          ),
+          height: 50,
+          child: TextField(
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.black87,
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Color(0xff5ac18e),
+              ),
+              hintText: 'Password',
+              hintStyle: TextStyle(),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildForgotPasswordButton() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: TextButton(
+        onPressed: () => print('Forgot password?'),
+        child: Text(
+          'Forgot password?',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildRememberMe() {
+    return Container(
+      height: 20,
+      child: Row(
+        children: [
+          Theme(
+            data: ThemeData(unselectedWidgetColor: Colors.white),
+            child: Checkbox(
+                value: isRememberMe,
+                checkColor: Colors.green,
+                activeColor: Colors.white,
+                onChanged: (value) {
+                  setState(() {
+                    isRememberMe = value;
+                  });
+                }),
+          ),
+          Text('Remember me',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ))
+        ],
+      ),
+    );
+  }
+
+  Widget buildLoginButton() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25),
+      width: double.infinity,
+      child: ElevatedButton(
+          onPressed: () => print('login....'),
+          child: Text(
+            'LOGIN',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+              Color(0xff5ac18e),
+            ),
+          )),
+      // child: RaisedButton(
+      //   onPressed: () => print('Login...'),
+      //   shape: RoundedRectangleBorder(
+      //     borderRadius: BorderRadius.circular(15),
+      //   ),
+      // //
+      //   color: Colors.white,
+      //   child: Text(
+      //     'LOGIN',
+      //     style: TextStyle(
+      //       color: Color(0xff5ac18e),
+      //       fontSize: 18,
+      //       fontWeight: FontWeight.bold,
+      //     ),
+      //   ),
+      // ),
+    );
+  }
+
+  Widget buildSignUpButton() {
+    return GestureDetector(
+      onTap: () => print("sign up...."),
+      child: RichText(
+        text: TextSpan(children: [
+          TextSpan(
+            text: 'Don\' have an account?   ',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          TextSpan(
+              text: 'Sign Up',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ))
+        ]),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,18 +218,50 @@ class _LoginState extends State<Login> {
         child: Stack(
           children: <Widget>[
             Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                      Color(0x665ac18e),
-                      Color(0x995ac18e),
-                      Color(0xcc5ac18e),
-                      Color(0xff5ac18e),
-                    ])))
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0x665ac18e),
+                    Color(0x995ac18e),
+                    Color(0xcc5ac18e),
+                    Color(0xff5ac18e),
+                  ],
+                ),
+              ),
+              child: SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 25,
+                  vertical: 120,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    buildEmail(),
+                    SizedBox(height: 20),
+                    buildPassword(),
+                    buildForgotPasswordButton(),
+                    buildLoginButton(),
+                    buildRememberMe(),
+                    SizedBox(height: 20),
+                    buildSignUpButton(),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
