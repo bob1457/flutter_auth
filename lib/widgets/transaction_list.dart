bae1676dd4a456721c4b79f98c6e16b6e0a7ada9
Bob <bob.yuan@yahooo.com>
2021-10-02 22:21:18 -0700

    Refacto using ListView

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -27,59 +27,67 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
+    return Container(
+      height: 400,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return _buildTransactionContent(index);
+        },
+        itemCount: transactions.length,
+      ),
+    );
+  }
+
+  Widget _buildTransactionContent(int index) {
+    return Card(
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 12,
+              horizontal: 16,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1,
+                color: Colors.black87,
+              ),
+            ),
+            padding: EdgeInsets.all(12),
+            child: Text(
+              // tx.amount.toString(),
+              '\$${transactions[index].amount}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.purple,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 12,
-                  horizontal: 16,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.black87,
-                  ),
-                ),
-                padding: EdgeInsets.all(12),
-                child: Text(
-                  // tx.amount.toString(),
-                  '\$${tx.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.purple,
-                    fontSize: 24,
-                  ),
+              Text(
+                transactions[index].title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    DateFormat.yMMMMd().format(tx.date),
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                DateFormat.yMMMMd().format(transactions[index].date),
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
               ),
             ],
           ),
-        );
-      }).toList(),
+        ],
+      ),
     );
   }
 }
