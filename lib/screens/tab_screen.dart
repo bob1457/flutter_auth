1b2b38f7d30f3bcb728d2bb9d6a8189098e5c0cd
Bob <bob.yuan@yahooo.com>
2022-02-27 16:04:26 -0800

    Replace the tab bar with bottom navigation bar

--- lib/screens/tab_screen.dart
+++ lib/screens/tab_screen.dart
@@ -12,35 +12,55 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Widget> _pages = [
+    CategoryScreen(),
+    FavoritesScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectedPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                  color: Colors.amber,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                  color: Colors.red,
-                ),
-                text: 'Favorates',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [CategoryScreen(), FavoritesScreen()],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+        // bottom: TabBar(
+        //   tabs: [
+        //     Tab(
+        //       icon: Icon(
+        //         Icons.category,
+        //         color: Colors.amber,
+        //       ),
+        //       text: 'Categories',
+        //     ),
+        //     Tab(
+        //       icon: Icon(
+        //         Icons.star,
+        //         color: Colors.red,
+        //       ),
+        //       text: 'Favorates',
+        //     ),
+        //   ],
+        // ),
+      ),
+      // body: TabBarView(
+      //   children: [CategoryScreen(), FavoritesScreen()],
+      // ),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectedPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), label: "CategoryCategories"),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites"),
+        ],
       ),
     );
   }
